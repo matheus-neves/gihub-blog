1aa9d76f32e2a6929706b7ba09d47ec26a75062c
Matheus Neves <matneves@SAO-C02G620KML7H.local>
2023-01-19 20:13:57 -0300

    setup project

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'src', 'components')
+      },
+      { find: '@styles', replacement: resolve(__dirname, 'src', 'styles') },
+      { find: '@pages', replacement: resolve(__dirname, 'src', 'pages') },
+      { find: '@assets', replacement: resolve(__dirname, 'src', 'assets') },
+    ]
+  },
+  server: {
+    host: true,
+    port: 3000
+  }
+});
