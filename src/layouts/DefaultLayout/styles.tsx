5ef1956c41e1f5ac2d0ff99093e3918cd998a29b
Matheus Neves <matheusneves1989@gmail.com>
2023-02-04 21:44:29 -0300

    feat(layouts/default-layout): add extra padding on container

--- src/layouts/DefaultLayout/styles.tsx
+++ src/layouts/DefaultLayout/styles.tsx
@@ -8,10 +8,10 @@ export const Container = styled.div`
   max-width: calc(864px + 2rem);
   height: 100%;
   margin: 0 auto;
-  padding: 0 1rem;
+  padding: 0 1rem 4rem;
 
   @media (min-width: ${({ theme }) => `${theme.breakpoints.sm}px`}) {
     max-width: calc(864px + 4rem);
-    padding: 0 2rem;
+    padding: 0 2rem 8rem;
   }
 `;
