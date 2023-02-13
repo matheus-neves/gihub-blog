ca5e5aaf5f66ecc47bec5a60564f2056525c303c
Matheus Neves <matheusneves1989@gmail.com>
2023-02-13 17:50:03 -0300

    refactor(components/card): change to tailwind

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,17 +1,56 @@
-import { CardContainer } from './styles';
+import { ReactNode } from 'react';
 
-export function Card() {
+function Card({ children, ...props }: { children: ReactNode }) {
   return (
-    <CardContainer>
-      <header>
-        <h3>JavaScript data types and data structures</h3>
-        <time>1 day ago</time>
-      </header>
-      <p>
-        Programming languages all have built-in data structures, but these often
-        differ from one language to another. This article attempts to list the
-        built-in data structures available in
-      </p>
-    </CardContainer>
+    <div
+      className="
+        flex
+        flex-col
+        gap-5
+        p-8
+        rounded-[10px]
+        bg-primary-base-post
+        border-2 
+        border-transparent 
+        cursor-pointer
+        transition
+        hover:border-2
+        hover:border-primary-base-label
+      
+        md:min-h-[260px]
+      "
+      {...props}
+    >
+      {children}
+    </div>
   );
 }
+
+interface HeaderProps {
+  title: string;
+  date: string;
+  created_at: string;
+}
+
+function Header({ date, title, created_at }: HeaderProps) {
+  return (
+    <header className="flex items-baseline flex-1 justify-between gap-5">
+      <h3 className="md:line-clamp-2 text-xl">{title}</h3>
+      <time
+        className="whitespace-nowrap text-sm text-right text-primary-base-span"
+        dateTime={created_at}
+      >
+        {date}
+      </time>
+    </header>
+  );
+}
+
+function Description({ children }: { children: ReactNode }) {
+  return <p className="line-clamp-4">{children}</p>;
+}
+
+Card.Header = Header;
+Card.Description = Description;
+
+export { Card };
