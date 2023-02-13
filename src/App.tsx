7435b143921b7765670fe7079953e74df431ff4d
Matheus Neves <matheusneves1989@gmail.com>
2023-02-13 18:24:16 -0300

    buid: remove styled-components. setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { router } from '@src/routes/Router';
-import { GlobalStyles } from '@styles/global';
-import { defaultTheme } from '@styles/themes/default';
+import '@styles/index.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 import { QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import { queryClient } from './lib/react-query';
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyles />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: 'var(--font-nunito-sans), sans-serif'
+      },
+      backgroundImage: {
+        'header-pattern': "url('@assets/cover.jpg')"
+      },
+      colors: {
+        primary: {
+          blue: '#3294F8',
+          'base-title': '#E7EDF4',
+          'base-subtitle': '#C4D4E3',
+          'base-text': '#AFC2D4',
+
+          'base-span': '#7B96B2',
+          'base-label': '#3A536B',
+          'base-border': '#1C2F41',
+          'base-post': '#112131',
+
+          'base-profile': '#0B1B2B',
+          'base-background': '#071422',
+          'base-input': '#040F1A',
+
+          white: '#FFFFFF'
+        }
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/line-clamp')]
+};
