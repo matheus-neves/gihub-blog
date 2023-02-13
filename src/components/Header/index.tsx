3c2993b2633efaa7cb746305d990b242631764ff
Matheus Neves <matheusneves1989@gmail.com>
2023-02-13 18:18:18 -0300

    refactor(components/header): change to tailwind

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,33 @@
 import logo from '@assets/logo.svg';
-import { HeaderContainer } from './styles';
 
 export function Header() {
   return (
-    <HeaderContainer>
-      <img src={logo} alt="" />
-    </HeaderContainer>
+    <header
+      className="
+        w-full
+        h-[150px]
+        bg-[length:100%100%]
+        bg-header-pattern
+        bg-no-repeat
+
+        flex
+        items-center
+        justify-center
+
+        md:h-[296px]
+      "
+    >
+      <img
+        src={logo}
+        alt=""
+        className="
+        h-[60px]
+        mt-[-30px]
+
+        md:h-[98px]
+        md:mt-[-50px]
+      "
+      />
+    </header>
   );
 }
