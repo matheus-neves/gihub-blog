96f8410452db627eca97f6c49c710ac966588446
Matheus Neves <matheusneves1989@gmail.com>
2023-02-13 18:18:53 -0300

    refactor(layouts/default-layout): change to tailwind

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,14 +1,29 @@
 import { Header } from '@src/components/Header';
 import { Outlet } from 'react-router-dom';
-import { Container } from './styles';
 
 export function DefaultLayout() {
   return (
     <>
       <Header />
-      <Container>
+      <div
+        className="
+          flex
+          flex-col
+          flex-1
+
+          max-w-[calc(864px+20px)]
+          h-full
+          m-auto
+          px-[10px]
+          pb-10
+
+          md:max-w-[calc(864px+40px)]
+          md:px-[20px]
+          md:pb-20
+        "
+      >
         <Outlet />
-      </Container>
+      </div>
     </>
   );
 }
