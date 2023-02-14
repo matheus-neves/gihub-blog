7b05f94147cd2e38574cf95c36a139cac530177d
Matheus Neves <matheusneves1989@gmail.com>
2023-02-14 14:15:51 -0300

    feat(pages/post): integrate header data

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -6,13 +6,55 @@ import {
   faComment
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { api } from '@src/lib/axios';
+import { getRelativeTime } from '@src/utils/getRelativeTime';
+import { useQuery } from 'react-query';
+import { Link, useParams } from 'react-router-dom';
 
-// https://api.github.com/repos/rocketseat-education/reactjs-github-blog-challenge/issues/1
+interface PostReturnType {
+  title: string;
+  comments: number;
+  updated_at: string;
+  formattedUpdatedAt: string;
+  html_url: string;
+  body: string;
+  name: string;
+}
+
 export function Post() {
+  const { number } = useParams();
+
+  console.log(number);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['issue'],
+    queryFn: async () => {
+      const response = await api.get(
+        `https://api.github.com/repos/rocketseat-education/reactjs-github-blog-challenge/issues/${number}`
+      );
+
+      const { title, comments, updated_at, html_url, body, user } =
+        response.data;
+
+      const formattedData = {
+        title,
+        comments,
+        formattedUpdatedAt: getRelativeTime(new Date(updated_at).getTime()),
+        html_url,
+        body,
+        name: user.login
+      } as PostReturnType;
+
+      return formattedData;
+    },
+    staleTime: 1000 * 60 * 60 * 24 // 24 hours
+  });
+
+  console.log(data);
   return (
-    <header
-      className="
+    <>
+      <header
+        className="
         flex
         flex-col
         bg-primary-base-profile
@@ -24,26 +66,26 @@ export function Post() {
         md:px-10
         md:mt-[-80px]
       "
-    >
-      <div className="flex items-center justify-between w-full mb-5">
-        <Link
-          to="/"
-          className="
+      >
+        <div className="flex items-center justify-between w-full mb-5">
+          <Link
+            to="/"
+            className="
             flex
             no-underline
             text-primary-blue
             border-b
             border-b-transparent
           "
-        >
-          <FontAwesomeIcon icon={faChevronLeft} width={10} height={10} />
-          <span className="uppercase ml-2 text-xs font-bold">Back</span>
-        </Link>
-        <a
-          href="https://api.github.com/repos/rocketseat-education/reactjs-github-blog-challenge/issues/1"
-          target="_blank"
-          rel="noreferrer"
-          className="
+          >
+            <FontAwesomeIcon icon={faChevronLeft} width={10} height={10} />
+            <span className="uppercase ml-2 text-xs font-bold">Back</span>
+          </Link>
+          <a
+            href={data?.html_url}
+            target="_blank"
+            rel="noreferrer"
+            className="
             flex
             
             no-underline
@@ -54,50 +96,54 @@ export function Post() {
             transition
             hover:border-primary-blue
           "
-        >
-          <span className="uppercase mr-2 text-xs font-bold">
-            See on github
-          </span>
-          <FontAwesomeIcon
-            icon={faArrowUpRightFromSquare}
-            fade
-            width={12}
-            height={12}
-          />
-        </a>
-      </div>
-      <div>
-        <h1 className="text-2xl mb-2">
-          JavaScript data types and data structures
-        </h1>
-        <ul className="flex flex-wrap flex-1 gap-3 md:gap-8">
-          <li className="flex items-center">
+          >
+            <span className="uppercase mr-2 text-xs font-bold">
+              See on github
+            </span>
             <FontAwesomeIcon
-              icon={faGithub}
-              className=" text-primary-base-label min-h-[18px]"
-              width={18}
-              height={18}
+              icon={faArrowUpRightFromSquare}
+              fade
+              width={12}
+              height={12}
             />
-            <span className="text-primary-base-span ml-2">Lorem</span>
-          </li>
-          <li className="flex items-center">
-            <FontAwesomeIcon
-              icon={faCalendarDay}
-              className=" text-primary-base-label min-h-[18px]"
-              width={18}
-              height={18}
-            />
-            <span className="text-primary-base-span ml-2">1 day ago</span>
-          </li>
-          <li className="flex items-center">
-            <FontAwesomeIcon
-              icon={faComment}
-              className=" text-primary-base-label min-h-[18px]"
-            />
-            <span className="text-primary-base-span ml-2">5 comments</span>
-          </li>
-        </ul>
-      </div>
-    </header>
+          </a>
+        </div>
+        <div>
+          <h1 className="text-2xl mb-2">{data?.title}</h1>
+          <ul className="flex flex-wrap flex-1 gap-3 md:gap-8">
+            <li className="flex items-center">
+              <FontAwesomeIcon
+                icon={faGithub}
+                className=" text-primary-base-label min-h-[18px]"
+                width={18}
+                height={18}
+              />
+              <span className="text-primary-base-span ml-2">{data?.name}</span>
+            </li>
+            <li className="flex items-center">
+              <FontAwesomeIcon
+                icon={faCalendarDay}
+                className=" text-primary-base-label min-h-[18px]"
+                width={18}
+                height={18}
+              />
+              <span className="text-primary-base-span ml-2">
+                {data?.formattedUpdatedAt}
+              </span>
+            </li>
+            <li className="flex items-center">
+              <FontAwesomeIcon
+                icon={faComment}
+                className=" text-primary-base-label min-h-[18px]"
+              />
+              <span className="text-primary-base-span ml-2">
+                {data?.comments} comments
+              </span>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <div>{data?.body}</div>
+    </>
   );
 }
