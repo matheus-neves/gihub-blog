797dff7bf9d273935ecfe730bbf54f402d9186a0
Matheus Neves <matheusneves1989@gmail.com>
2023-02-15 16:37:41 -0300

    feat(router): add param :number on post route

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
-        path: '/post',
+        path: '/post/:number',
         element: <Post />
       }
     ]
